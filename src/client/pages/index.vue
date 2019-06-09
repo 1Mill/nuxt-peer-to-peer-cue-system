cce52ffaad47afe20ab14be22d36bb9eeefd6c57
1Mill <erik.ekberg@outlook.com>
2019-06-09 10:40:42 -0700

    Scaffold sender and receiver pages

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-	<h1>Hello world!</h1>
+	<div>
+		<h1>Nuxt peer-to-peer Cue example</h1>
+
+		<v-btn
+		:key='link'
+		:to='`/${link}`'
+		color='primary'
+		large
+		target='_blank'
+		v-for='link in links'
+		>
+			{{ link }}
+		</v-btn>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			links: [
+				'receiver',
+				'sender',
+			],
+		}
+	}
+}
+</script>
+
---
+++ src/client/pages/receiver.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<h1>Reciever</h1>
+	</div>
+</template>
