44353defc8357363f5880ddd4b413e1c1bfd80fc
1Mill <erik.ekberg@outlook.com>
2019-06-09 20:27:28 -0700

    Send signals to peers

--- src/client/pages/sender.vue
+++ src/client/pages/sender.vue
@@ -1,18 +1,38 @@
 <script>
+const SIGNALS = [
+	'fade',
+	'go',
+	'off',
+	'reset',
+]
+
 export default {
 	data() {
 		return {
+			SIGNALS,
+			activeSignal: null,
+			message: 'Waiting for input ...',
 			peerId: null,
 		}
 	},
 	methods: {
 		joinPeer() {
 			if (this.peerId) {
-				const conn = this.$peer.connect(this.peerId.trim())
+				this.$peer.conn = this.$peer.connect(this.peerId.trim(), { reliable: true })
 
-				conn.on('open', () => {
-					conn.send('Sending a new message')
+				this.$peer.conn.on('open', () => {
+					this.message = `Connected to peer ${this.peerId}`
+
+					this.$peer.conn.send('Sending a new message')
 				})
+			}
+		},
+		signalPeer(signal) {
+			const { conn } = this.$peer
+
+			if (conn.open) {
+				conn.send(signal)
+				this.activeSignal = signal
 			}
 		},
 	},
@@ -37,6 +57,29 @@ export default {
 					small
 					>
 						Connect
+					</v-btn>
+				</v-flex>
+				<v-flex>
+					{{ message }}
+				</v-flex>
+				<v-flex>
+					<v-btn
+					@click='signalPeer'
+					>
+						Signaling
+					</v-btn>
+				</v-flex>
+			</v-layout>
+			<v-layout>
+				<v-flex
+				:key='signal'
+				v-for='signal in SIGNALS'
+				>
+					<v-btn
+					:color='`${ signal === activeSignal ? "info" : "accent" }`'
+					@click='signalPeer(signal)'
+					>
+						{{ signal }}
 					</v-btn>
 				</v-flex>
 			</v-layout>
